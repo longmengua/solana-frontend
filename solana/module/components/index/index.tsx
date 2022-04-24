fcf9d7e2b1395feb3a775b58527d8beafc48dc49
Waltor <longmengua@gmail.com>
2022-04-25 03:06:39 +0800

    get metadata

--- solana/module/components/index/index.tsx
+++ solana/module/components/index/index.tsx
@@ -220,10 +220,20 @@ export const Index = () => {
   const getMetadata = async () => {
     if (!publicKey) throw new WalletNotConnectedError();
 
-    const response: RpcResponseAndContext<Array<{pubkey: PublicKey; account: AccountInfo<ParsedAccountData>;}>> = await connection.getParsedTokenAccountsByOwner(publicKey, { programId: TOKEN_PROGRAM_ID });
-    const accounts: Array<{pubkey: PublicKey; account: AccountInfo<ParsedAccountData>;}> = response.value;
+    const seed = 'metadata';
+    const nftTokenAddress = 'YYNd7xcEAJumWghGf5UVqzgwefARYST5UDYM6mUsrZN';
+    const metaplexAddress = 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s';
 
-    console.log('response', accounts)
+    const [PDA] = await PublicKey.findProgramAddress(
+      [Buffer.from(seed), new PublicKey(metaplexAddress).toBuffer(), new PublicKey(nftTokenAddress).toBuffer()],
+      new PublicKey(metaplexAddress),
+    );
+
+    const nftMeta = await connection.getAccountInfo(PDA);
+    if(!nftMeta?.data?.buffer) throw new Error('missing data')
+    const data = Buffer.from(nftMeta?.data?.buffer)
+  
+    console.log('nftMeta', data.toString())
   }
 
   const getSupply = async () => {
